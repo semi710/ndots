55b28f1ff435640984c8ad6b829dbd967e288f7d
Nikhil Singh <nik.singh710@gmail.com>
2026-05-18 18:34:40 +0530

    hm(darwin/jp-mbp): auto-fix nix.conf to use extra- prefix
    Add activation hook that converts bare substituters/trusted-public-keys
    to extra-substituters/extra-trusted-public-keys in ~/.config/nix/nix.conf.
    This prevents cachix use and similar tools from overriding system config.

--- hosts/darwin/jp-mbp/users/nikhil.singh.nix
+++ hosts/darwin/jp-mbp/users/nikhil.singh.nix
@@ -131,4 +131,30 @@ in
       )
     }
   '';
+
+  home.activation.fix-nix-conf-extra-prefix = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    nixConfFile="${config.home.homeDirectory}/.config/nix/nix.conf"
+
+    if [[ -f "$nixConfFile" ]]; then
+      if $DRY_RUN_CMD grep -qE '^[[:space:]]*(substituters|trusted-public-keys)[[:space:]]*=' "$nixConfFile"; then
+        $VERBOSE_ECHO "Converting ~/.config/nix/nix.conf list settings to extra- prefix..."
+
+        tmpFile="''$nixConfFile.tmp.$$"
+        while IFS= read -r line || [[ -n "$line" ]]; do
+          if [[ "$line" =~ ^[[:space:]]*substituters[[:space:]]*= ]] && \
+             ! [[ "$line" =~ ^[[:space:]]*extra-substituters[[:space:]]*= ]]; then
+            $DRY_RUN_CMD echo "$line" | ${pkgs.gnused}/bin/sed 's/^[[:space:]]*substituters/extra-substituters/' >> "$tmpFile"
+          elif [[ "$line" =~ ^[[:space:]]*trusted-public-keys[[:space:]]*= ]] && \
+               ! [[ "$line" =~ ^[[:space:]]*extra-trusted-public-keys[[:space:]]*= ]]; then
+            $DRY_RUN_CMD echo "$line" | ${pkgs.gnused}/bin/sed 's/^[[:space:]]*trusted-public-keys/extra-trusted-public-keys/' >> "$tmpFile"
+          else
+            $DRY_RUN_CMD echo "$line" >> "$tmpFile"
+          fi
+        done < "$nixConfFile"
+
+        $DRY_RUN_CMD mv "$tmpFile" "$nixConfFile"
+        $VERBOSE_ECHO "Done: nix.conf list settings now use extra- prefix"
+      fi
+    fi
+  '';
 }
