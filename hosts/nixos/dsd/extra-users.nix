559b46e34b4e8ba2c576d7c9ca847504481fec37
Nikhil Singh <nik.singh710@gmail.com>
2026-05-21 19:15:41 +0530

    feat(user/dsd): added extra-users
    To be purged from the semi

--- hosts/nixos/dsd/extra-users.nix
+++ hosts/nixos/dsd/extra-users.nix
@@ -1,5 +1,10 @@
 let
-  users = { };
+  users = {
+    "sunny.sehwag" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB9si+8/vDKLDskxjbmcGy/B6rKaU5M5D9E+eSQtwu3T";
+    "charana.c" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOjfGad/qfh3IavYYhD//1o7cYdbiBwzBfVCcawIMaTe";
+    "aditya.dubey" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOsdusr1XztYyE/8C1xQ9ECvXjbFPQllhkUcAXf+f4sx";
+    "jashan.singh" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPQ6P/mQtgM4i3sGaW8lQEiIhBsOlmE8v69RCV6TBf6D";
+  };
 
   mkUsers = usersFromKeys: {
     nix.settings.trusted-users = builtins.attrNames usersFromKeys;
