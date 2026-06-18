53f59c7cf6889f54ed6e6135549691fcca38eb49
Nikhil Singh <nikhil@semi.sh>
2026-06-19 02:49:44 +0530

    feat(dsd): add mantej.tuli SSH key

--- hosts/nixos/dsd/extra-users.nix
+++ hosts/nixos/dsd/extra-users.nix
@@ -5,6 +5,7 @@ let
     "aditya.dubey" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOsdusr1XztYyE/8C1xQ9ECvXjbFPQllhkUcAXf+f4sx";
     "jashan.singh" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPQ6P/mQtgM4i3sGaW8lQEiIhBsOlmE8v69RCV6TBf6D";
     "sudhir.singh" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ0saed1dIwIkR31UulQ/V6ojyQeKkOrAjJ7xgjnVIkp";
+    "mantej.tuli" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIALnxpuEy0YlAVgtFVcIE2ivH+LfgPsgok3fxtqrnFTc";
   };
 
   mkUsers = usersFromKeys: {
