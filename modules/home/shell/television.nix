c333a30b8d381b3cd654f31b31e84f7958455408
Nikhil Singh <nikhil@semi.sh>
2026-07-07 01:48:23 +0530

    feat(television): vim-style keymaps, drop ctrl-a (tmux prefix conflict)
    - ctrl-j/k: next/prev entry (vim down/up)
    - ctrl-d/u: half-page preview scroll (vim normal mode)
    - ctrl-y: yank to clipboard
    - ctrl-g: toggle help (remapped from ctrl-h which is backspace)
    - ctrl-a: dropped (tmux prefix C-a intercepts it)
    - ctrl-n/p: next/prev entry (vim-style)
    - ctrl-w: delete prev word (vim-style)
    - ctrl-e: go to input end (readline/vim)
    - home/end: input navigation (no ctrl-a needed)

--- modules/home/shell/television.nix
+++ modules/home/shell/television.nix
@@ -1,19 +1,70 @@
-{ ... }:
+{ flake, pkgs, ... }:
+let
+  nixvimOptions =
+    flake.inputs.nixvim.packages.${pkgs.system}.options-json + "/share/doc/nixos/options.json";
+in
 {
   programs = {
-    television.enable = true;
+    television = {
+      enable = true;
+      settings.keybindings = {
+        esc = "quit";
+        ctrl-c = "quit";
+
+        ctrl-n = "select_next_entry";
+        ctrl-p = "select_prev_entry";
+        down = "select_next_entry";
+        up = "select_prev_entry";
+
+        ctrl-d = "scroll_preview_half_page_down";
+        ctrl-u = "scroll_preview_half_page_up";
+        pagedown = "scroll_preview_half_page_down";
+        pageup = "scroll_preview_half_page_up";
+
+        enter = "confirm_selection";
+        tab = "toggle_selection_down";
+        backtab = "toggle_selection_up";
+
+        ctrl-y = "copy_entry_to_clipboard";
+        ctrl-r = "reload_source";
+        ctrl-s = "cycle_sources";
+        ctrl-f = "cycle_previews";
+
+        ctrl-t = "toggle_remote_control";
+        ctrl-x = "toggle_action_picker";
+        ctrl-o = "toggle_preview";
+        ctrl-g = "toggle_help";
+        f12 = "toggle_status_bar";
+        ctrl-l = "toggle_layout";
+
+        backspace = "delete_prev_char";
+        ctrl-w = "delete_prev_word";
+        delete = "delete_next_char";
+        left = "go_to_prev_char";
+        right = "go_to_next_char";
+        home = "go_to_input_start";
+        end = "go_to_input_end";
+        ctrl-e = "go_to_input_end";
+
+        ctrl-up = "select_prev_history";
+        ctrl-down = "select_next_history";
+      };
+    };
 
     nix-search-tv = {
       enable = true;
       enableTelevisionIntegration = true;
-      settings.indexes = [
-        "nixpkgs"
-        "home-manager"
-        "nixos"
-        "darwin"
-        "nur"
-        "noogle"
-      ];
+      settings = {
+        indexes = [
+          "nixpkgs"
+          "home-manager"
+          "nixos"
+          "darwin"
+          "nur"
+          "noogle"
+        ];
+        experimental.options_file.nixvim = "${nixvimOptions}";
+      };
     };
   };
 
