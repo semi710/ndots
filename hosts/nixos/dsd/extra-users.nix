14c901124752fa72b3d0543e982378156a09c2bf
Nikhil Singh <nikhil@semi.sh>
2026-07-29 14:44:41 +0530

    fix(dsd): update charana.c ssh key

--- hosts/nixos/dsd/extra-users.nix
+++ hosts/nixos/dsd/extra-users.nix
@@ -1,7 +1,7 @@
 let
   users = {
     "sunny.sehwag" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB9si+8/vDKLDskxjbmcGy/B6rKaU5M5D9E+eSQtwu3T";
-    "charana.c" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOjfGad/qfh3IavYYhD//1o7cYdbiBwzBfVCcawIMaTe";
+    "charana.c" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILoFTzP1jCYcGX7KwJu0QrnKlWhOUCTbhftEPadNyQBB";
     "aditya.dubey" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOsdusr1XztYyE/8C1xQ9ECvXjbFPQllhkUcAXf+f4sx";
     "jashan.singh" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPQ6P/mQtgM4i3sGaW8lQEiIhBsOlmE8v69RCV6TBf6D";
     "sudhir.singh" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ0saed1dIwIkR31UulQ/V6ojyQeKkOrAjJ7xgjnVIkp";
